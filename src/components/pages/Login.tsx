cbfaa369d3e0d1b17b389821ba0bde9c8edd3da3
Tomoko-hub <haisaitomoco@gmail.com>
2024-03-13 08:01:43 +0200

    install axios

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,15 +1,23 @@
-import { memo,FC } from "react"
-import { Box, Divider, Flex, Heading, Input, Button, Stack } from "@chakra-ui/react";
+import { memo,FC, useState } from "react"
+import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
+import { PrimaryButton } from "../atoms/buttons/PrimaryButton";
 
 const Login:FC = memo(()=>{
+    const [userId, setUserId] = useState('');
+    const onChangeUserId =(e:ChangeEvent<HTMLInputElement>)=>setUserId(e.target.value);
+
     return(
         <Flex align="center" justify="center" height="100vh">
             <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
                 <Heading as="h1" size="lg" textAlign="center">User managiment App</Heading>
                 <Divider my={4} />
                 <Stack spacing={6} py={4} px={10}>
-                    <Input placeholder="User ID" />
-                    <Button bg="teal.400" color="white" _hover={{opacity: 0.8}}>Login</Button>
+                    <Input 
+                    placeholder="User ID" 
+                    value={userId}
+                    onChange={onChangeUserId}
+                     />
+                    <PrimaryButton>Login</PrimaryButton>
                 </Stack>
             </Box>
         </Flex>
