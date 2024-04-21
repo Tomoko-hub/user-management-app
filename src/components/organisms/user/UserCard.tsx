4d7112e51b1906b09ceeca25648c77da9f259322
Tomoko-hub <haisaitomoco@gmail.com>
2024-04-21 20:07:02 +0300

    create user.tsx

---
+++ src/components/organisms/user/UserCard.tsx
@@ -0,0 +1,48 @@
+import { FC, memo } from "react";
+import { Box, Stack, Image, Text } from "@chakra-ui/react";
+
+type Props = {
+    imageUrl: string;
+    username: string;
+    fullName: string;
+};
+
+export const UserCard:FC<Props>= memo((props)=>{
+    const { imageUrl,username,fullName } = props;
+    return(
+        <Box 
+            w='260px' 
+            h='260px' 
+            bg='white' 
+            borderRadius='10px' 
+            shadow='md'
+            padding={4}
+            _hover={{ 
+                cursor: 'pointer', 
+                opacity: 0.8
+            }}
+            >
+            <Stack textAlign='center'>
+                <Image
+                    borderRadius='full' 
+                    boxSize='160px' 
+                    src={imageUrl}
+                    alt={username}
+                    margin='auto'
+                     />
+                <Text 
+                    fontSize='lg'
+                    fontWeight='bold'
+                >
+                {username}
+                </Text>
+                <Text
+                    fontSize='sm'
+                    color='gray'
+                >
+                {fullName}
+                </Text>
+            </Stack>
+        </Box>
+    )
+})
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,7 +1,15 @@
 import { memo,FC } from "react"
+import { Wrap, WrapItem } from "@chakra-ui/react";
+import { UserCard } from "../organisms/user/UserCard";
 
 const UserManagement:FC = memo(()=>{
-    return<p>UserManagement</p>
-})
+    return(
+        <Wrap p={{ base: 4, md:10 }}>
+            <WrapItem>
+                <UserCard imageUrl={"https://picsum.photos/160"} username='Tomokoman' fullName='tomoko uehara' />
+            </WrapItem>
+        </Wrap>
+    );
+});
 
 export default UserManagement;
